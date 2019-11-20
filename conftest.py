68d6d7f34a694aa6fb95e346c5842685d21dfdf9
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-11-20 18:23:06 +0000

    added the chromedriver path on the conftest.py and updated the pytest.ini

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ from pytest import fixture
 
 from selenium import webdriver
 
-@fixture(scope='function')
+@fixture(scope='session')
 def chrome_browser():
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome('/Applications/chromedriver')
     return browser
